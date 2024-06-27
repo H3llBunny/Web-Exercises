dc795085d8032116d87428a0c29f121bb548fa79
H3llBunny <bogo.dishlyanov@gmail.com>
2024-06-27 08:49:24 +0300

    Added a simple chat using signalR

---
+++ SignalRSimpleChat/SignalRSimpleChat/Hug/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRSimpleChat.Models.Chat;
+
+namespace SignalRSimpleChat.Hug
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync(
+                "NewMessage",
+                new Message { User = this.Context.User.Identity.Name, Text = message });
+        }
+    }
+}
